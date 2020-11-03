fa01ba782e093582fd6df09be2e660e76e2484df
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-11-03 22:35:22 +0100

    Add window properties

--- general/src/LoginWindow.java
+++ general/src/LoginWindow.java
@@ -5,7 +5,9 @@ public class LoginWindow extends JFrame {
     public LoginWindow(String s){
         super(s);
         setSize(600,400);
-
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //Termine l'application à la fermeture de la fenêtre
+        setLocationRelativeTo(null); //Centre la fenêtre
+        setResizable(false); //Empêche le redimensionnement de la fenêtre
 
         setVisible(true);
     }
--- general/src/principal.java
+++ general/src/principal.java
@@ -2,6 +2,6 @@ public class principal {
     public static LoginWindow login;
 
     public static void main(String[] args) {
-        login = new LoginWindow("Menu principal");
+        login = new LoginWindow("Authentification");
     }
 }
