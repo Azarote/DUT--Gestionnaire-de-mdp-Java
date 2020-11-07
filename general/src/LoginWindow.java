b32fb2f11e9d1f50cca1f359c13d74df73e801a5
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-11-07 23:17:56 +0100

    visual for LoginWindow

--- general/src/LoginWindow.java
+++ general/src/LoginWindow.java
@@ -4,10 +4,24 @@ public class LoginWindow extends JFrame {
 
     public LoginWindow(String s){
         super(s);
-        setSize(600,400);
+        setSize(500,220);
         setDefaultCloseOperation(EXIT_ON_CLOSE); //Termine l'application à la fermeture de la fenêtre
         setLocationRelativeTo(null); //Centre la fenêtre
         setResizable(false); //Empêche le redimensionnement de la fenêtre
+
+        setLayout(null);
+
+        JLabel enterPassword = new JLabel("Saisir le mot de passe");
+        enterPassword.setBounds(105,30,290,25);
+        add(enterPassword);
+
+        JPasswordField enterPasswordField = new JPasswordField();
+        enterPasswordField.setBounds(105,50,290,25);
+        add(enterPasswordField);
+
+        JButton validatePassword = new JButton("Valider");
+        validatePassword.setBounds(105,90,150,25);
+        add(validatePassword);
 
         setVisible(true);
     }
