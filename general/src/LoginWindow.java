33881a608f0e63cf4e6029649ce3fd289b3ad788
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-11-08 10:07:48 +0100

    Few comments
    + reduce height of SignUpWindow from 400px to 370px

--- general/src/LoginWindow.java
+++ general/src/LoginWindow.java
@@ -8,17 +8,19 @@ public class LoginWindow extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE); //Termine l'application à la fermeture de la fenêtre
         setLocationRelativeTo(null); //Centre la fenêtre
         setResizable(false); //Empêche le redimensionnement de la fenêtre
-
         setLayout(null);
 
+        //Label demandant d'entrer le mot de passe
         JLabel enterPassword = new JLabel("Saisir le mot de passe");
         enterPassword.setBounds(105,30,290,25);
         add(enterPassword);
 
+        //Champ de texte pour saisir le mot de passe
         JPasswordField enterPasswordField = new JPasswordField();
         enterPasswordField.setBounds(105,50,290,25);
         add(enterPasswordField);
 
+        //Bouton pour valider l'opération
         JButton validatePassword = new JButton("Valider");
         validatePassword.setBounds(105,90,150,25);
         add(validatePassword);
--- general/src/SignUpWindow.java
+++ general/src/SignUpWindow.java
@@ -11,34 +11,38 @@ public class SignUpWindow extends JFrame {
 
     public SignUpWindow(String a){
         super(a);
-        setSize(600,400);
+        setSize(600,370);
         setDefaultCloseOperation(EXIT_ON_CLOSE); //Termine l'application à la fermeture de la fenêtre
         setLocationRelativeTo(null); //Centre la fenêtre
         setResizable(false); //Empêche le redimensionnement de la fenêtre
-
-
         setLayout(null);
 
+        //Label demandant d'entrer un nouveau mot de passe
         JLabel newPassword = new JLabel("Nouveau mot de passe");
         newPassword.setBounds(150,30,290,25);
         add(newPassword);
 
+        //Champ de texte pour saisir le nouveau mot de passe
         JPasswordField passwordField = new JPasswordField();
         passwordField.setBounds(150,50,290,25);
         add(passwordField);
 
+        //Label demandant de confirmer le nouveau mot de passe
         JLabel newPasswordConfirmation = new JLabel("Confirmer le nouveau mot de passe");
         newPasswordConfirmation.setBounds(150,80,300,25);
         add(newPasswordConfirmation);
 
+        //Champ de texte pour confirmer le nouveau mot de passe
         JPasswordField passwordFieldConfirmation = new JPasswordField();
         passwordFieldConfirmation.setBounds(150,100,290,25);
         add(passwordFieldConfirmation);
 
+        //Bouton pour valider l'opération
         JButton validateButton = new JButton("Valider");
         validateButton.setBounds(150,140,150,25);
         add(validateButton);
 
+        //Labels affichant les critères à respecter pour un mot de passe valide
         JLabel label1 = new JLabel("ATTENTION", SwingConstants.CENTER);
         label1.setFont(new Font("Helvetica", Font.PLAIN, 17));
         label1.setForeground(Color.RED);
@@ -64,7 +68,5 @@ public class SignUpWindow extends JFrame {
         add(label4);
 
         setVisible(true);
-
-
     }
 }
