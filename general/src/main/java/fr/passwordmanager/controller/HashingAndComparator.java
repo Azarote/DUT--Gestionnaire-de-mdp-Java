9539ca5dca4b368daa9380d6669e630b03f24179
MUNOZ Matteo <matteo.munoz@etu.univ-amu.fr>
2020-11-17 17:05:35 +0100

    Add Hashing password and store it on a file

--- general/src/main/java/fr/passwordmanager/controller/HashingAndComparator.java
+++ general/src/main/java/fr/passwordmanager/controller/HashingAndComparator.java
@@ -2,15 +2,26 @@ package fr.passwordmanager.controller;
 
 import com.google.common.hash.Hashing;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class HashingAndComparator {
 
-    public static void mainPasswordHashing(char[] mainPassword)  {
+    public static void mainPasswordHashing(char[] mainPassword) {
         final String hashed = Hashing.sha256()
                 .hashString(String.valueOf(String.valueOf(mainPassword)), StandardCharsets.UTF_8)
                 .toString();
-
+        File hashedFile = new File("general/src/hashed.dat");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(hashedFile));
+            writer.write(hashed);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
