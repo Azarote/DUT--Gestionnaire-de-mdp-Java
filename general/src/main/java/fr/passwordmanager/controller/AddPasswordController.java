4e152c61a62a16e1b417cf1f94947d1720ce53c7
MUNOZ Matteo <matteo.munoz@etu.univ-amu.fr>
2020-11-21 23:57:21 +0100

    add controller of addpassword

---
+++ general/src/main/java/fr/passwordmanager/controller/AddPasswordController.java
@@ -0,0 +1,8 @@
+package fr.passwordmanager.controller;
+
+public class AddPasswordController {
+   public static void AddPasswordProcessing(String titre, String username, char[] password, String URL, String description, String expiration_date){
+
+
+   }
+}
